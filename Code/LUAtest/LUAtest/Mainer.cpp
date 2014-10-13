bfdc3ad9d156a7f7500b00914ab66e827e13be93
Anthony Corbin <dragon1672@gmail.com>
2014-10-12 23:21:10 -0600

    Working LUA example with classes

--- Code/LUAtest/LUAtest/Mainer.cpp
+++ Code/LUAtest/LUAtest/Mainer.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#pragma warning( push )
+#pragma warning( disable : 4244 )
 #include <luacppinterface.h>
+#pragma  warning( pop )
 /*
 	Lua lua;
 	bool LoadLua() {
@@ -114,25 +117,37 @@ bool LoadLua() {
 }
 bool luaLoaded = LoadLua();
 
+//represents class from external lib that I cannot edit
 class Vec {
 public:
 	float x,y,z;
 };
-class LuaVec : public Vec {
-public:
-	float getX() { return x; }
-	float getY() { return y; }
-	float getZ() { return z; }
 
-	float setX(float toSet) { x = toSet; }
-	float setY(float toSet) { y = toSet; }
-	float setZ(float toSet) { z = toSet; }
+//binding class to wrap not sure how else to approach this
+//since I need getters and setters
+class LuaVec {
+	Vec& base;
+public:
+	LuaVec(Vec& base) : base(base) { }
+	float getX() { return base.x; }
+	float getY() { return base.y; }
+	float getZ() { return base.z; }
+
+	void setX(float toSet) { base.x = toSet; }
+	void setY(float toSet) { base.y = toSet; }
+	void setZ(float toSet) { base.z = toSet; }
 };
 
-LuaUserdata<Vec> convertVec(Vec& toConvert) {
-	auto ret = lua.CreateUserdata<Vec>(&toConvert);
+LuaUserdata<LuaVec> convertVec(Vec& toConvert) {
+	auto ret = lua.CreateUserdata<LuaVec>(new LuaVec(toConvert));
 	
-	ret.Bind("x",&Vec::x);
+	ret.Bind("getX",&LuaVec::getX);
+	ret.Bind("getY",&LuaVec::getY);
+	ret.Bind("getZ",&LuaVec::getZ);
+
+	ret.Bind("setX",&LuaVec::setX);
+	ret.Bind("setY",&LuaVec::setY);
+	ret.Bind("setZ",&LuaVec::setZ);
 
 	return ret;
 }
@@ -143,34 +158,38 @@ public:
 	Vec rot;
 public:
 	Vec& getPos()   { return pos;   }
-	LuaUserdata<Vec> getLuaPos()   { return convertVec(pos); }
+	LuaUserdata<LuaVec> getLuaPos()   { return convertVec(pos); }
 	Vec& getScale() { return scale; }
-	LuaUserdata<Vec> getLuaScale() { return convertVec(scale); }
+	LuaUserdata<LuaVec> getLuaScale() { return convertVec(scale); }
 	Vec& getRot()   { return rot;   }
-	LuaUserdata<Vec> getLuaRot()   { return convertVec(rot); }
+	LuaUserdata<LuaVec> getLuaRot()   { return convertVec(rot); }
+
+	static LuaUserdata<Matrix> getLuaVersion(Matrix& toConvert) {
+		auto ret = lua.CreateUserdata<Matrix>(&toConvert);
+		ret.Bind("getPos",  &Matrix::getLuaPos);
+		ret.Bind("getScale",&Matrix::getLuaScale);
+		ret.Bind("getRot",  &Matrix::getLuaRot);
+		return ret;
+	}
 };
-LuaUserdata<Matrix> convertMat(Matrix& toConvert) {
-	auto ret = lua.CreateUserdata<Matrix>(&toConvert);
-	ret.Bind("getPos",  &Matrix::getLuaPos);
-	ret.Bind("getScale",&Matrix::getLuaScale);
-	ret.Bind("getRot",  &Matrix::getLuaRot);
-	return ret;
-}
 
 
 class Entity {
 public:
 	Matrix mat;
-	Matrix getMat();
-	LuaUserdata<Matrix> getLuaMat() { return convertMat(mat); }
+	Matrix& getMat() { return mat; }
+	LuaUserdata<Matrix> getLuaMat() { return Matrix::getLuaVersion(mat); }
+	static LuaUserdata<Entity> getLuaVersion(Entity& toConvert) {
+		return getLuaVersion(&toConvert);
+	}
+	static LuaUserdata<Entity> getLuaVersion(Entity * toConvert) {
+		auto ret = lua.CreateUserdata<Entity>(toConvert);
+		ret.Bind("getMat", &Entity::getLuaMat);
+		return ret;
+	}
 	static void registerToLua() {
 		auto constructor = lua.CreateFunction<LuaUserdata<Entity>()>(std::bind([](Lua lua) -> LuaUserdata<Entity> {
-			auto foo = new Entity();
-			auto userData = lua.CreateUserdata<Entity>(foo);
-
-			userData.Bind("getMat", &Entity::getLuaMat);
-
-			return userData;
+			return Entity::getLuaVersion(new Entity());
 		}, lua));
 		LuaTable classObj = lua.CreateTable();
 		classObj.Set("new", constructor);
@@ -181,15 +200,25 @@ public:
 void main() {
 	Entity::registerToLua();
 
+	Entity a;
+
+	lua.GetGlobalEnvironment().Set("a",Entity::getLuaVersion(a));
+
 	std::string err = lua.RunScript(""
-		"local a = Entity:new();\n"
+		//"local a = Entity:new();\n"
 		"print(a.getMat().getPos())\n"
-		"print(a.getMat().getPos().x)\n"
-		"\n"
+		"print(a.getMat().getPos().getX())\n"
+		"a.getMat().getPos().setX(5)\n"
+		"print(a.getMat().getPos().getX())\n"
+		"local b = 5;\n"
 		"\n"
 		"\n"
 		"\n"
 		"\n"
 		"");
 	std::cout << err << std::endl;
+	//auto test = lua.GetGlobalEnvironment().Get<float>("a.getMat().getPos().getX()");
+	//auto test = lua.GetGlobalEnvironment().Get<float>("b");
+	//std::cout << test << std::endl;
+	std::cout << a.mat.pos.x << std::endl;
 }
