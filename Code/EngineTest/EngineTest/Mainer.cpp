e283d117b3bcb4e14d549997d78a48afbec89384
Anthony Corbin <dragon1672@gmail.com>
2014-10-17 03:35:44 -0600

    Example testing APIs

--- Code/EngineTest/EngineTest/Mainer.cpp
+++ Code/EngineTest/EngineTest/Mainer.cpp
@@ -3,45 +3,90 @@
 #include <Engine/Entity/Entity.h>
 #include <Engine/Entity/Component.h>
 #include <Engine/Entity/Components/ScriptComponent.h>
+#include <Engine/Tools/MatrixInfo.h>
+#include <Engine/Systems/GameObjectManager.h>
 
-
+#include <iostream>
 int main(int argc, char * argv[]) {
 	//QApplication app(argc, argv);
 	//_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	Entity e;
-	e.addComponent(new ScriptComponent());
-	e.getComponent<ScriptComponent>()->script = ""
-		"context = {}                                          \n"
-		"--keep all vars within scope of context               \n"
-		"context.exampleVar = 5;                               \n"
-		"                                                      \n"
-		"--called first                                        \n"
-		"context.start = function()                            \n"
-		"    print(context.exampleVar)                         \n"
-		"    return true                                       \n"
-		"end                                                   \n"
-		"                                                      \n"
-		"--updates are called after start                      \n"
-		"context.earlyUpdate = function()                      \n"
-		"    context.exampleVar = context.exampleVar + 1;      \n"
-		"    return true                                       \n"
-		"end                                                   \n"
-		"context.update = function()                           \n"
-		"    print(context.exampleVar)                         \n"
-		"    return true                                       \n"
-		"end                                                   \n"
-		"context.lateUpdate = function()                       \n"
-		"    --yay                                             \n"
-		"    return true                                       \n"
-		"end                                                   \n"
-		"";
-	e.init();
-	for (int i = 0; i < 2; i++) {
-		e.earlyUpdate();
-		e.update();
-		e.lateUpdate();
+	GameObjectManager game;
+
+	Entity * e = game.AddEntity("Steve");
+	e->addComponent(new MatrixInfo());
+	e->getTrans()->pos.x = -10;
+	std::string scripts[] = {
+		//*hello world
+		"context.exampleVar = 5                                     \n"
+		"context.start = function()                                 \n"
+		"    print(context.exampleVar)                              \n"
+		"    return true                                            \n"
+		"end                                                        \n"
+		"                                                           \n"
+		"context.earlyUpdate = function()                           \n"
+		"    context.exampleVar = context.exampleVar + 1;           \n"
+		"    return true                                            \n"
+		"end                                                        \n"
+		"context.update = function()                                \n"
+		"    print(context.exampleVar)                              \n"
+		"    return true                                            \n"
+		"end                                                        \n"
+		"context.lateUpdate = function() return true end            \n"
+		"",
+
+		//*/
+		//*random
+		"context.start = function() return true end                 \n"
+		"                                                           \n"
+		"context.earlyUpdate = function() return true end           \n"
+		"context.update = function()                                \n"
+		"    local x = context.parent.getTrans().pos().getX();      \n"
+		"    x = x + Random.RangeFloat(1,3)                         \n"
+		"    context.parent.getTrans().pos().setX(x);               \n"
+		"    return true                                            \n"
+		"end                                                        \n"
+		"context.lateUpdate = function()                            \n"
+		"    print('x'..context.parent.getTrans().pos().getX())     \n"
+		"    return true                                            \n"
+		"end                                                        \n"
+		"",
+		//*/
+		//*dt
+		"context.start = function() return true end                 \n"
+		"                                                           \n"
+		"context.earlyUpdate = function() return true end           \n"
+		"context.update = function()                                \n"
+		"    print('dt: '..Timer.deltaTime())                       \n"
+		"    return true                                            \n"
+		"end                                                        \n"
+		"context.lateUpdate = function() return true end            \n"
+		"",
+		//*/
+		/*accessing script
+		"context.start = function() return true; end                \n"
+		"    return true                                            \n"
+		"end                                                        \n"
+		"                                                           \n"
+		"context.earlyUpdate = function()                           \n"
+		"    context.parent.getScript().context().parent.getTrans().pos().setX(-8) \n"
+		"    return true                                            \n"
+		"end                                                        \n"
+		"context.update = function() return true end                \n"
+		"context.lateUpdate = function() return true end            \n"
+		//*/ //*
+	};
+	for (int i = 0; i < sizeof(scripts)/sizeof(*scripts); i++)
+	{
+		e->addComponent(new ScriptComponent(scripts[i].c_str()));
 	}
+	e->getScript();
+	game.init();
+	e->getScript();
+	for (int i = 0; i < 5; i++) {
+		game.update();
+	}
+	std::cout << e->getTrans()->pos.x << std::endl;
 
 	return 0;//app.exec();
 }
