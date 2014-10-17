7032f56cf8b709e64aa5d6f76ebe3911927069de
Anthony Corbin <dragon1672@gmail.com>
2014-10-16 18:56:37 -0600

    Updated #defines

--- Code/LUAtest/LUAtest/Mainer.cpp
+++ Code/LUAtest/LUAtest/Mainer.cpp
@@ -335,8 +335,8 @@ public:
 
 #define MAKE_LUA_INSTANCE_RET(class_name,varname) LuaUserdata<class_name> varname = lua.CreateUserdata<class_name>((##class_name##*)this,[](class_name*){}) //this disables the destructor call
 
-#define GET_LUA_VER_PTR(var_type,real_var) LuaUserdata<var_type>get_lua_##real_var##() const { return (LuaUserdata<var_type>)(*real_var); }
-#define GET_LUA_VER(var_type,real_var) LuaUserdata<var_type>get_lua_##real_var##() const { return (LuaUserdata<var_type>)real_var; }
+#define GET_LUA_VER_PTR(var_type,real_var) LuaUserdata<var_type>get_lua_##real_var##() { return (LuaUserdata<var_type>)(*real_var); }
+#define GET_LUA_VER(var_type,real_var) LuaUserdata<var_type>get_lua_##real_var##() { return (LuaUserdata<var_type>)real_var; }
 #define BIND_LUA_VER(class_name,var_name,real_var) var_name.Bind(#real_var,  &##class_name##::get_lua_##real_var  )
 
 class WrapVec3 {
