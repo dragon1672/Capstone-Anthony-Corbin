3fd5a1b17b45a22c91cb7003d3743ba9ccbf0651
Anthony Corbin <dragon1672@gmail.com>
2014-10-20 15:35:34 -0600

    Using Resource Manager

--- Code/EngineTest/EngineTest/Mainer.cpp
+++ Code/EngineTest/EngineTest/Mainer.cpp
@@ -7,6 +7,7 @@
 #include <Engine/Systems/GameObjectManager.h>
 
 #include <iostream>
+#include <Engine/Tools/Random/StringRandom.h>
 int main(int argc, char * argv[]) {
 	//QApplication app(argc, argv);
 	//_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
@@ -16,30 +17,23 @@ int main(int argc, char * argv[]) {
 	Entity * e = game.AddEntity("Steve");
 	e->addComponent<MatrixInfo>();
 	e->getTrans()->pos.x = -10;
-	std::string scripts[] = {
-		//*hello world
+	Script * scripts[] = {
+		resourceManager.addScript_src("name",//hello world
 		"function context:start()                           \n"
 		"    self.exampleVar = 5;                           \n"
 		"    return true                                    \n"
 		"end                                                \n"
 		"                                                   \n"
-		"function context:myPrint(a) print(a..' example') end\n"
-		"                                                   \n"
-		"function context:earlyUpdate() self:myPrint('Early') return true end     \n"
 		"function context:update()                          \n"
 		"    self.exampleVar = self.exampleVar + 1;         \n"
 		"    print('Herro: '..self.exampleVar)              \n"
 		"    self:myPrint();                                \n"
 		"    return true                                    \n"
 		"end                                                \n"
-		"function context:lateUpdate() self:myPrint('late') return true end     \n"
-		"",
-	
-		//*/
-		//*random
-		"function context:myPrint(a) print(a..' random') end\n"
-		"                                                   \n"
-		"function context:earlyUpdate() self:myPrint('Early') return true end     \n"
+		""),
+
+
+		resourceManager.addScript_src("name",//random
 		"function context:update()                           \n"
 		"    local x = self.parent.getTrans().pos().getX();  \n"
 		"    x = x + Random.RangeFloat(1,3)                  \n"
@@ -48,41 +42,45 @@ int main(int argc, char * argv[]) {
 		"    print(Random.String.Letters(5)..':'..self.parent.getTrans().pos().getX()) \n"
 		"    return true                                     \n"
 		"end                                                 \n"
-		"function context:lateUpdate() self:myPrint('late') return true end     \n"
-		"",
-		//*/
-		//*dt
-		"function context:myPrint(a) print(a..' dt') end\n"
-		"                                                   \n"
-		"function context:earlyUpdate() self:myPrint('Early') return true end     \n"
+		""),
+
+		resourceManager.addScript_src("name",//dt
 		"function context:update()                         \n"
 		"    print('dt: '..Timer.deltaTime())              \n"
 		"    return true                                   \n"
 		"end                                               \n"
-		"function context:lateUpdate() self:myPrint('late') return true end     \n"
-		"",
-		//*/
-		/*accessing script -- broken
-		"function context:start() return true end                                \n"
-		"                                                                        \n"
+		""),
+
+		resourceManager.addScript_src("name",//accessing script -- broken
 		"function context:earlyUpdate()                                          \n"
 		"    self.parent.getScript().context().parent.getTrans().pos().setX(-8)  \n"
 		"    return true                                                         \n"
 		"end                                                                     \n"
-		"context.update = function() return true end                             \n"
-		"function context:lateUpdate() return true end                           \n"
-		"",
-		//*/ //*
+		""),
 	};
+	int size = sizeof(scripts)/sizeof(*scripts);
+	size--; // skipping the last one
+
+	Entity * f = game.AddEntity("Bob");
+	f->addComponent<MatrixInfo>();
+	f->getTrans()->pos.x = -10;
 	for (int i = 0; i < sizeof(scripts)/sizeof(*scripts); i++)
 	{
-		e->addComponent<ScriptComponent>(scripts[i].c_str());
+		e->addComponent<ScriptComponent>(scripts[i]->getID());
+		f->addComponent<ScriptComponent>(scripts[i]->getID());
 	}
-	game.init();
-	for (int i = 0; i < 5; i++) {
-		game.update();
+	//game.init();
+	//for (int i = 0; i < 5; i++) {
+	//	std::cout << std::endl;
+	//	game.update();
+	//}
+	//std::cout << e->getTrans()->pos.x << std::endl;
+
+
+	for (int i = 0; i < 1000; i++)
+	{
+		std::cout << Random::rString::Letters(5) << std::endl;
 	}
-	std::cout << e->getTrans()->pos.x << std::endl;
 	
 	return 0;//app.exec();
 }
