99203df9db76deb3ef23e104c343ac516d3c480e
Anthony Corbin <dragon1672@gmail.com>
2014-10-20 19:00:39 -0600

    main now calls GUI

--- Code/EngineTest/EngineTest/Mainer.cpp
+++ Code/EngineTest/EngineTest/Mainer.cpp
@@ -1,86 +1,174 @@
-//#include <QtWidgets/QApplication>
-//#include <Engine/DebugTools/DebugMemHeader.h>
-#include <Engine/Entity/Entity.h>
-#include <Engine/Entity/Component.h>
-#include <Engine/Entity/Components/ScriptComponent.h>
-#include <Engine/Tools/MatrixInfo.h>
-#include <Engine/Systems/GameObjectManager.h>
+#include <QtWidgets/QApplication>
+#include <../Engine/Engine/DebugTools/DebugMemHeader.h>
+#include <../Engine/Engine/Entity/Entity.h>
+#include <../Engine/Engine/Entity/Component.h>
+#include <../Engine/Engine/Entity/Components/ScriptComponent.h>
+#include <../Engine/Engine/Tools/MatrixInfo.h>
+#include <../Engine/../Engine/../Engine/../Engine/../Engine/../Engine/../Engine/../Engine/../Engine/../Engine/../Engine/Engine/Systems/GameObjectManager.h>
 
+#include "BasicGui.h"
+
+#include <ShapeGenerator.h>
+
+#pragma region shaderCode
+const char * defaultVertShader = "#version 400					  \n"
+	"															  \n"
+	"in layout(location=0) vec3 pos;							  \n"
+	"in layout(location=1) vec3 col;							  \n"
+	"in layout(location=3) vec2 uv;								  \n"
+	"															  \n"
+	"out vec2 fragUv;											  \n"
+	"out vec3 fragCol;											  \n"
+	"out vec3 fragPos;											  \n"
+	"out vec3 fragWorldPos;										  \n"
+	"															  \n"
+	"//mats														  \n"
+	"uniform mat4x4 viewTransform;								  \n"
+	"uniform mat4x4 model2WorldTransform;						  \n"
+	"															  \n"
+	"void main() {												  \n"
+	"	vec4 transformedPos = model2WorldTransform * vec4(pos,1); \n"
+	"	gl_Position = viewTransform * transformedPos;			  \n"
+	"	fragUv = uv;											  \n"
+	"	fragCol = col;											  \n"
+	"	fragPos = pos;											  \n"
+	"	fragWorldPos = vec3(transformedPos);					  \n"
+	"}															  \n"
+	"															  \n";
+//color
+const char * defaultFragShader_Color = "#version 400\n"
+	"												\n"
+	"in vec3 fragCol;								\n"
+	"												\n"
+	"void main() {									\n"
+	"	gl_FragColor = vec4(fragCol,1);				\n"
+	"}												\n"
+	"";
+//texture
+const char * defaultFragShader_Texture = "#version 400\n"
+	"												  \n"
+	"in vec2 fragUv;								  \n"
+	"uniform sampler2D myTexture;					  \n"
+	"												  \n"
+	"void main() {									  \n"
+	"	gl_FragColor = texture(myTexture,fragUv);	  \n"
+	"}												  \n"
+	"";
+//Cube map withModeldPos
+const char * defaultFragShader_CubeMapModel= "#version 400\n"
+	"													  \n"
+	"in vec3 fragPos;									  \n"
+	"uniform samplerCube worldMap;						  \n"
+	"													  \n"
+	"void main() {										  \n"
+	"	gl_FragColor = texture(worldMap,fragPos);		  \n"
+	"}													  \n"
+	"";
+//Cube map withWorldPos
+const char * defaultFragShader_CubeMapWorld = "#version 400\n"
+	"													   \n"
+	"in vec3 fragWorldPos;								   \n"
+	"uniform samplerCube worldMap;						   \n"
+	"													   \n"
+	"void main() {										   \n"
+	"	gl_FragColor = texture(worldMap,fragWorldPos);	   \n"
+	"}													   \n"
+	"													   \n";
+#pragma endregion
+
+int main(int argc, char * argv[]) {
+	QApplication app(argc, argv);
+	BasicGui gui;
+
+	resourceManager.addMesh("cube",Neumont::ShapeGenerator::makeCube());
+	//resourceManager.addShader_file()
+
+	gui.init();
+
+	gui.show();
+
+	gui.startGameLoop();
+
+
+	return app.exec();
+}
+
+//Script Testing
+/*
 #include <iostream>
 #include <Engine/Tools/Random/StringRandom.h>
 int main(int argc, char * argv[]) {
-	//QApplication app(argc, argv);
-	//_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	GameObjectManager game;
-	
-	Entity * e = game.AddEntity("Steve");
-	e->addComponent<MatrixInfo>();
-	e->getTrans()->pos.x = -10;
-	Script * scripts[] = {
-		resourceManager.addScript_src("name",//hello world
-		"function context:start()                           \n"
-		"    self.exampleVar = 5;                           \n"
-		"    return true                                    \n"
-		"end                                                \n"
-		"                                                   \n"
-		"function context:update()                          \n"
-		"    self.exampleVar = self.exampleVar + 1;         \n"
-		"    print('Herro: '..self.exampleVar)              \n"
-		"    self:myPrint();                                \n"
-		"    return true                                    \n"
-		"end                                                \n"
-		""),
+GameObjectManager game;
+
+Entity * e = game.AddEntity("Steve");
+e->addComponent<MatrixInfo>();
+e->getTrans()->pos.x = -10;
+Script * scripts[] = {
+resourceManager.addScript_src("name",//hello world
+"function context:start()                           \n"
+"    self.exampleVar = 5;                           \n"
+"    return true                                    \n"
+"end                                                \n"
+"                                                   \n"
+"function context:update()                          \n"
+"    self.exampleVar = self.exampleVar + 1;         \n"
+"    print('Herro: '..self.exampleVar)              \n"
+"    self:myPrint();                                \n"
+"    return true                                    \n"
+"end                                                \n"
+""),
 
 
-		resourceManager.addScript_src("name",//random
-		"function context:update()                           \n"
-		"    local x = self.parent.getTrans().pos().getX();  \n"
-		"    x = x + Random.RangeFloat(1,3)                  \n"
-		"    self.parent.getTrans().pos().setX(x);           \n"
-		"                                                    \n"
-		"    print(Random.String.Letters(5)..':'..self.parent.getTrans().pos().getX()) \n"
-		"    return true                                     \n"
-		"end                                                 \n"
-		""),
+resourceManager.addScript_src("name",//random
+"function context:update()                           \n"
+"    local x = self.parent.getTrans().pos().getX();  \n"
+"    x = x + Random.RangeFloat(1,3)                  \n"
+"    self.parent.getTrans().pos().setX(x);           \n"
+"                                                    \n"
+"    print(Random.String.Letters(5)..':'..self.parent.getTrans().pos().getX()) \n"
+"    return true                                     \n"
+"end                                                 \n"
+""),
 
-		resourceManager.addScript_src("name",//dt
-		"function context:update()                         \n"
-		"    print('dt: '..Timer.deltaTime())              \n"
-		"    return true                                   \n"
-		"end                                               \n"
-		""),
+resourceManager.addScript_src("name",//dt
+"function context:update()                         \n"
+"    print('dt: '..Timer.deltaTime())              \n"
+"    return true                                   \n"
+"end                                               \n"
+""),
 
-		resourceManager.addScript_src("name",//accessing script -- broken
-		"function context:earlyUpdate()                                          \n"
-		"    self.parent.getScript().context().parent.getTrans().pos().setX(-8)  \n"
-		"    return true                                                         \n"
-		"end                                                                     \n"
-		""),
-	};
-	int size = sizeof(scripts)/sizeof(*scripts);
-	size--; // skipping the last one
+resourceManager.addScript_src("name",//accessing script -- broken
+"function context:earlyUpdate()                                          \n"
+"    self.parent.getScript().context().parent.getTrans().pos().setX(-8)  \n"
+"    return true                                                         \n"
+"end                                                                     \n"
+""),
+};
+int size = sizeof(scripts)/sizeof(*scripts);
+size--; // skipping the last one
 
-	Entity * f = game.AddEntity("Bob");
-	f->addComponent<MatrixInfo>();
-	f->getTrans()->pos.x = -10;
-	for (int i = 0; i < sizeof(scripts)/sizeof(*scripts); i++)
-	{
-		e->addComponent<ScriptComponent>(scripts[i]->getID());
-		f->addComponent<ScriptComponent>(scripts[i]->getID());
-	}
-	//game.init();
-	//for (int i = 0; i < 5; i++) {
-	//	std::cout << std::endl;
-	//	game.update();
-	//}
-	//std::cout << e->getTrans()->pos.x << std::endl;
-
-
-	for (int i = 0; i < 1000; i++)
-	{
-		std::cout << Random::rString::Letters(5) << std::endl;
-	}
-	
-	return 0;//app.exec();
+Entity * f = game.AddEntity("Bob");
+f->addComponent<MatrixInfo>();
+f->getTrans()->pos.x = -10;
+for (int i = 0; i < sizeof(scripts)/sizeof(*scripts); i++)
+{
+e->addComponent<ScriptComponent>(scripts[i]->getID());
+f->addComponent<ScriptComponent>(scripts[i]->getID());
 }
+//game.init();
+//for (int i = 0; i < 5; i++) {
+//	std::cout << std::endl;
+//	game.update();
+//}
+//std::cout << e->getTrans()->pos.x << std::endl;
+
+
+for (int i = 0; i < 1000; i++)
+{
+std::cout << Random::rString::Letters(5) << std::endl;
+}
+
+return 0;//app.exec();
+}
+//*/
